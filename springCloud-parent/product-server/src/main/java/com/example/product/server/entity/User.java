295347046eb61dc1807be4b2122fb11a9264fbc4
yuchao <2298229166@qq.com>
2020-05-14 11:52:36 +0800

    yc

---
+++ springCloud-parent/product-server/src/main/java/com/example/product/server/entity/User.java
@@ -0,0 +1,21 @@
+package com.example.product.server.entity;
+
+import lombok.Data;
+
+/**
+ * Created by Administrator on 2019/6/18.
+ */
+@Data
+public class User {
+
+    private Integer id;
+
+    private String name;
+
+    private String sex;
+
+    private String phone;
+
+    private Integer teacherId;
+
+}
